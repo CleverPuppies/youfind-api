69bd15dabc5ba7432b4e087288b98d98a42f6882
migueldo <mail.deoliveiramiguel@gmail.com>
2022-12-09 15:13:18 +0800

    removed the use of secrets.yml in rakefile

--- spec/helpers/spec_helper.rb
+++ spec/helpers/spec_helper.rb
@@ -14,7 +14,6 @@ require_relative '../../require_app'
 require_app
 
 VIDEO_ID = '8bZh5LMaSmE'
-CONFIG = YAML.safe_load(File.read('config/secrets.yml'))
 YT_API_KEY = YouFind::App.config.API_KEY
 CORRECT = YAML.safe_load(File.read('spec/fixtures/yt_results.yml'))
 VIDEO_URL = "http://youtube.com/watch?v=#{VIDEO_ID}".freeze
